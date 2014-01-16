944446ba326bb06f2cb15efecf3b0b22d802a75c
Conlin Durbin <conlindurbin@gmail.com>
2014-01-16 12:44:55 -0500

    Added Giphy feature

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ bot.addListener("message", function(from, to, text, message) {
 	}
 
 	if(text.substr(0,6)=="!help") {
-		bot.say(from, "Hi "+from+"! Here are my commands\n!motd - display the current DN MOTD\n!weather <zip,city,location> - tells you the weather in a location.\n!help - displays this help dialog\n!btc - returns the current bitcoin price\n!feature <feature request> - request a feature for the bot\n!set dribbble <username> - set your dribbble username\n!dribbble - return your most recent followed shot (must have !set dribbble <username> before using)\n!gifme <term> - returns a gif related to the term");
+		bot.say(from, "Hi "+from+"! Here are my commands\n!motd - display the current DN MOTD\n!weather <zip,city,location> - tells you the weather in a location.\n!help - displays this help dialog\n!btc - returns the current bitcoin price\n!feature <feature request> - request a feature for the bot\n!set dribbble <username> - set your dribbble username\n!dribbble - return your most recent followed shot (must have !set dribbble <username> before using)\n!gif <id> - get gif by id from Giphy\n!gifme <term> - returns a gif related to the term");
 	}
 
 	if(text.substr(0,5)=="!btc") {
