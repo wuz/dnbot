050ca594da601922211a9e5785b07c01e1ecd4d5
Conlin Durbin <conlindurbin@gmail.com>
2014-03-21 23:03:58 -0400

    Added github command

--- app.js
+++ app.js
@@ -76,6 +76,16 @@ bot.addListener("message", function(from, to, text, message) {
 		});
 	}
 
+	if(text.substr(0,6)=="!github") {
+		var url = 'https://github.com/trending';
+
+		request(url, function(err, resp, body){
+			$ = cheerio.load(body);
+			console.log($);
+			// bot.say(config.channels[0], "DN MOTD: "+$('.MOTDMessageContent p').text().replace(/^\s+|\s+$/g,'')+" "+$('.MOTDCourtesy').text().replace(/^\s+|\s+$/g,''));
+		});
+	}
+
 	if(text.substr(0,6)=="!help") {
 		bot.say(from, "Hi "+from+"! Here are my commands\n!motd - display the current DN MOTD\n!weather <zip,city,location> - tells you the weather in a location.\n!help - displays this help dialog\n!btc - returns the current bitcoin price\n!feature <feature request> - request a feature for the bot\n!set <setting> <option> - set various settings. Options: dribbble <username> - set your dribbble username. website <url> set your personal website.\n!dribbble - return your most recent followed shot (must have !set dribbble <username> before using)\n!gif <id> - get gif by id from Giphy\n!gifme <term> - returns a gif related to the term\n!gifsearch <term> - search for gif id's by term. Returned as PM.\n !whois <user> - return information set by a user with the !set command");
 	}
